4fbb7136962282b4b69df6ac5f9ef16c36a53afe
Nizar-Osman <117791265+Nizar-Osman@users.noreply.github.com>
2024-05-02 20:06:02 -0400

    rendered navbar to header component successfully!

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,33 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
 function Header() {
   return (
-    <div>
-      <nav className="navbar">
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Healthy Home Duct Cleaning</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Why</Nav.Link>
+            <NavDropdown title="Services" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Contact</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Online Quote
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Book Now</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
